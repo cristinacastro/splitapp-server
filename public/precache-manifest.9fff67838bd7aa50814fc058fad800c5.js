5ec81b7c399c86dcc8dc9fd3f0a6334eca208b3b
marcurto <mcurtogilabert@gmail.com>
2020-11-26 13:59:44 +0100

    deploy

--- public/precache-manifest.9fff67838bd7aa50814fc058fad800c5.js
+++ public/precache-manifest.9fff67838bd7aa50814fc058fad800c5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a86f6454b3da4e125fd67fbaeb2198d7",
+    "revision": "fdd3d917f329daa295ad541c95504c55",
     "url": "/index.html"
   },
   {
-    "revision": "f0a8a1f65158a88519a5",
-    "url": "/static/css/main.fd86538f.chunk.css"
+    "revision": "79a1785f31165bad7695",
+    "url": "/static/css/main.df4fad01.chunk.css"
   },
   {
     "revision": "915a4da024efcd24cf26",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.40b763be.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f0a8a1f65158a88519a5",
-    "url": "/static/js/main.24fe26d7.chunk.js"
+    "revision": "79a1785f31165bad7695",
+    "url": "/static/js/main.5b9a9cec.chunk.js"
   },
   {
     "revision": "5179ff928fc7dafdbfe9",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9badd7d15d33efdecd7deb8c37aec249.js"
+  "/precache-manifest.9fff67838bd7aa50814fc058fad800c5.js"
 );
 
 self.addEventListener('message', (event) => {
